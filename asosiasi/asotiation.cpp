460834d0287aeed7da7d0f8f495ebdb15eb44e27
lulultfh <punyalulu05@gmail.com>
2024-05-21 16:55:50 +0700

    add method procedure pasien untuk tambahDokter dan cetakDokter

--- asosiasi/asotiation.cpp
+++ asosiasi/asotiation.cpp
@@ -32,3 +32,13 @@ public:
     void tambahPasien(pasien*);
     void cetakPasien();
 };
+
+void pasien::tambahDokter(dokter* pDokter) {
+    daftar_dokter.push_back(pDokter);
+}
+void pasien::cetakDokter?() {
+    cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+    for (auto& a : daftar-dokter) {//auto digunakan dalam perulangan for untuk secara otomatis menentukan tipe data dari elemen yang diiteraas
+    }
+    cout << endl;
+}

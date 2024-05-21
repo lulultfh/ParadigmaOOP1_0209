f3a84f411393d3412859fd899b9a2fcf5254f5dd
lulultfh <punyalulu05@gmail.com>
2024-05-21 16:45:11 +0700

    add class pasien beserta membernya

--- asosiasi/asotiation.cpp
+++ asosiasi/asotiation.cpp
@@ -3,3 +3,16 @@
 using namespace std;
 
 class dokter;
+class pasien { //asosiasi
+public:
+    string nama;
+    vector<dokter*> daftar_dokter;
+    pasien(string pNama) :nama(pNama) {
+        cout << "pasien \"" << nama << "\" ada\n";
+    }
+    ~pasien() {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    }
+    void tambahDokter(dokter*);
+    void cetakDokter();
+};

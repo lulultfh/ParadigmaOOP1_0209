0cae93dbf0260afe4b950d7ed012083b0dca3584
lulultfh <punyalulu05@gmail.com>
2024-05-21 15:40:59 +0700

    add class manusia

--- composition/manusia.h
+++ composition/manusia.h
@@ -1,3 +1,19 @@
 #ifndef MANUSIA_H
 #define MANUSIA_h
 using namespace std;
+
+class manusia {
+    public:
+    string nama;
+    jantung varJantung;
+
+    manusia(string pNama): name(pName) {
+        cout << name << "hidup\n";
+
+    }
+    ~manusia() {
+        cout << name << "mati\n";
+
+    }
+}
+#endif

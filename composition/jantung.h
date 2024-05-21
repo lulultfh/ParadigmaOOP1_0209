9bf6dc8613b2e33e50c4731dc18fa40c962d443b
lulultfh <punyalulu05@gmail.com>
2024-05-21 15:37:46 +0700

    add class jantung dengan member konstruktor dan destruktor

--- composition/jantung.h
+++ composition/jantung.h
@@ -1,2 +1,14 @@
 #ifndef JANTUNG_H
 #define JANTUNG_H
+
+class jantung {
+    public: 
+    jantung() {
+        cout << "jantung dihidupkan\n";
+
+    }
+    ~jantung() {
+        cout << "Jantung dimatikan\n";
+    }
+};
+#endif
